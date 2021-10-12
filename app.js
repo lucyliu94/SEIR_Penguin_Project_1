c8ef225b10576d156a5003ae8083bd3c9303f320
Lucy Liu <lucy.liu94@gmail.com>
2021-10-12 11:52:45 -0400

    followed video to make game and first stab at styling + responsiveness

---
+++ app.js
@@ -0,0 +1,80 @@
+/******/
+//APP STATE
+/******/
+
+const state = {
+    player1: 0,
+    player2: 0,
+    currentQuestion: {},
+    which: true
+}
+
+let questions = []
+
+/*********** */
+//Main DOM Element
+/*********** */
+
+const $question = $("#question")
+const $a = $("#a")
+const $b = $("#b")
+const $c = $("#c")
+const $d = $("#d")
+const $p1score = $("#player-1 h4")
+const $p2score = $("#player-2 h4")
+
+/*********** */
+//Function
+/*********** */
+
+
+const chooseAnswer = (event, question) => {
+    console.log(event)
+    if (event.target.innerText === question.answer){
+        if (state.which){
+            state.player1++
+            state.which =!state.which
+        } else {
+            state.player2++
+            state.which =!state.which
+        }
+        setBoard(questions)
+    } else {
+        setBoard(questions)
+        state.which =!state.which
+    }
+}
+ 
+ 
+
+const setBoard = (q) => {
+    //Getting a random question
+    const randomIndex = Math.floor(Math.random() * q.length)
+    const randomQuestion = q[randomIndex]
+
+    $question.text(randomQuestion.question)
+    $a.text(randomQuestion.a)
+    $b.text(randomQuestion.b)
+    $c.text(randomQuestion.c)
+    $d.text(randomQuestion.d)
+
+    //update players scores
+    $p1score.text(state.player1)
+    $p2score.text(state.player2)
+    
+    $("li").off()
+    $("li").on("click", (event)=>{
+        chooseAnswer(event, randomQuestion)
+    })
+
+} 
+
+const URL = "https://cdn.contentful.com/spaces/fjqzeesozto5/environments/master/entries?access_token=CQ2_DeawViiarL0EFip65aLzs-NMdd7GYTSFUEWNjl4&content_type=triviaq"
+$.ajax(URL)
+.then((data) =>{
+    questions = data.items.map((q) => q.fields)
+    console.log(data)
+    console.log(questions)
+
+    setBoard(questions)
+}) 
